eca612437a91040d92aa3c1e1de402a5241c9741
Adam-0704 <adamchino8@gmail.com>
2025-05-13 10:40:48 +0200

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Velkommen til Roskilde Dyreinternat" );
+
+            bool running = true;
+
+            while(running == true){
+
+
+                /* administrere medarbejder/Kunder */
+               /* overblik over dyr, */
+               /* oprette og rediger dyr */
+
+
+
+
+
+
+
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+        }
+    }
+}
