8cb55176f2fb1cbb8c7704f6df441926645bd1b4
Patrick-LD <patricklykkedahl@gmail.com>
2025-05-13 12:04:58 +0200

    har tilføjet properties og constuctor til dyr class

--- ClassLibrary/dyr.cs
+++ ClassLibrary/dyr.cs
@@ -8,6 +8,22 @@ namespace ClassLibrary
 {
     internal class Dyr
     {
+        public string Art { get; set; }
+        public string Race { get; set; }
+        public string Navn { get; set; }
+        public string Chipnummer { get; set; }
+        public string Køn { get; set; }
+        public int Fødselsaar { get; set; }
 
+        // Constructor
+        public Dyr(string art, string race, string navn, string chipnummer, string køn, int fødselsaar)
+        {
+            Art = art;
+            Race = race;
+            Navn = navn;
+            Chipnummer = chipnummer;
+            Køn = køn;
+            Fødselsaar = fødselsaar;
+        }
     }
 }
