b96be3814894ec8f62dea8b670e99afb69e98b39
Adam-0704 <adamchino8@gmail.com>
2025-05-21 09:16:42 +0200

    tilføjet en del til razor page

--- RazorPage/Pages/SeAlleBruger.cshtml.cs
+++ RazorPage/Pages/SeAlleBruger.cshtml.cs
@@ -1,19 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Security.Cryptography;
+using ClassLibrary;
+using ClassLibrary.repository;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace RazorPage.Pages
 {
     public class PrivacyModel : PageModel
     {
-        private readonly ILogger<PrivacyModel> _logger;
-
-        public PrivacyModel(ILogger<PrivacyModel> logger)
-        {
-            _logger = logger;
-        }
+         public List<Bruger> BrugerListe { get; set; } = new();
 
         public void OnGet()
         {
+            BrugerRepository Repo = new BrugerRepository();
+
+            if (Repo.ErTom())
+            {
+                Repo.TilføjBruger(new Medarbejder("adam", "adam@gmail.com", "12345678"));
+                Repo.TilføjBruger(new Kunde("mohib", "mohib@gmail.com", "87654321"));
+            }
+            BrugerListe = Repo.HentAlleBrugere();
         }
     }
 
